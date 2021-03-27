7305c6c99128ed77baf2ac159255eb294ba191e8
wangzhuang <1243959690@qq.com>
2021-03-27 19:30:49 +0800

    update

---
+++ demo/src/main/java/com/my/blogs/example/dao/UserMapper.java
@@ -0,0 +1,8 @@
+package com.my.blogs.example.dao;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.my.blogs.example.dao.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+
+}
